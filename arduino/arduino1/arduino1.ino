f1d99b247e9bf5c362624663d40ebc65c229dcf7
DailyLollipops <clarencemadrigal84@gmail.com>
2023-12-06 02:59:08 +0800

    Refactored Arduino (for testing)

---
+++ arduino/arduino1/arduino1.ino
@@ -0,0 +1,251 @@
+#include "max6675.h"
+#include <Wire.h>
+#include <Servo.h>
+#include "HX711.h"
+
+#define NUM_R2 2
+
+HX711 scale1(3, 2);
+HX711 scale2(12, 13);
+Servo myservo;
+
+float calibrationFactor1 = -412;
+float calibrationFactor2 = -440;
+
+// LED
+const int greenLed = A5;
+const int redLed = 22;
+
+// Fan
+const int fanRelay1 = 10;
+const int fanRelay2 = 11;
+
+// Buzzer
+const int buzzer = 7;
+
+const int enaPin = A0; // the Arduino pin connected to the EN1 pin L298N
+const int in1Pin = 6;  // the Arduino pin connected to the IN1 pin L298N
+const int in2Pin = 5;  // the Arduino pin connected to the IN2 pin L298N
+
+// Thermo
+const int thermoDO = A1;
+const int thermoCS = A2;
+const int thermoCLK = A3;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+// Heater and Fan
+const int inArr[NUM_R2] = {8, 9};
+
+int current_command = -1;
+
+void setup() {
+  Serial.begin(9600);
+
+  scale1.set_scale(calibrationFactor1);
+  scale2.set_scale(calibrationFactor2);
+  scale1.tare();
+  scale2.tare();
+
+  // Actuator 
+  pinMode(enaPin, OUTPUT);
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+
+  digitalWrite(enaPin, HIGH);
+
+  myservo.attach(4);
+
+  // Buzzer 
+  pinMode(buzzer, OUTPUT);
+
+  // LED 
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+
+  // LED FAN
+  pinMode(fanRelay1, OUTPUT);
+  pinMode(fanRelay2, OUTPUT);
+
+  for (int i = 0; i < NUM_R2; i++) {
+    pinMode(inArr[i], OUTPUT);
+  }
+  for (int i = 0; i < NUM_R2; i++) {
+    digitalWrite(inArr[i], HIGH);
+  }
+}
+
+void loop() {
+  if (current_command == -1) {
+    receiveCommand();
+  } 
+  
+  else if (current_command == 11) {
+    getTemp();
+    current_command = -1;
+  } 
+  
+  else if (current_command == 12) {
+    getMoisture();
+    current_command = -1;
+  } 
+  
+  else if (current_command == 13) {
+    getWeight(1);
+    current_command = -1;
+  } 
+
+  else if (current_command == 14) {
+    getWeight(2);
+    current_command = -1;
+  } 
+
+  else if (current_command == 15) {
+    extendActuator();
+    current_command = -1;
+  } 
+
+  else if (current_command == 16) {
+    retractActuator();
+    current_command = -1;
+  } 
+  
+  else if (current_command == 17) {
+    turnOnFan();
+    current_command = -1;
+  } 
+  
+  else if (current_command == 18) {
+    turnOffFan();
+    current_command = -1;
+  }
+
+  else if (current_command == 19) {
+    activateBuzzer();
+    current_command = -1;
+  } 
+
+  else if (current_command == 20) {
+    signalGreenLed();
+    current_command = -1;
+  } 
+
+  else if (current_command == 21) {
+    signalRedLed();
+    current_command = -1;
+  } 
+
+  else if (current_command == 22) {
+    spinServo();
+    current_command = -1;
+  } 
+
+  else {
+    current_command = -1;
+  }
+}
+
+void sendResponse(String response) {
+  Serial.println(response);
+}
+
+void receiveCommand() {
+  if (Serial.available()) {
+    int sent = Serial.readStringUntil('\n').toInt();
+    Serial.println("ok");
+    current_command = sent;
+  }
+}
+
+void getTemp() {
+  float heater = thermocouple.readCelsius();
+  if (heater >= 30) {
+    for (int i = 0; i < NUM_R2; i++) {
+      digitalWrite(inArr[i], LOW);
+    }
+  } else if (heater >= 45) {
+    for (int i = 0; i < NUM_R2; i++) {
+      digitalWrite(inArr[i], HIGH);
+    }
+  }
+  sendResponse(String(heater));
+}
+
+void getMoisture() {
+  int airValue = 620; 
+  int waterValue = 310; 
+  int soilMoistureValue = analogRead(A4);
+  int soilMoisturePercent = map(soilMoistureValue, airValue, waterValue, 0, 100);
+  sendResponse(String(soilMoisturePercent));
+}
+
+void getWeight(int scale) {
+  float units = 0.00;
+  if(scale == 1) {
+    units = scale1.get_units(), 10;
+  }
+  else if(scale == 2) {
+    units = scale1.get_units(), 10;
+  }
+  else {
+    units = scale1.get_units(), 10;
+  }
+
+  if (units < 0) {
+    units = 0.00;
+  }
+  float weight = units * 0.001;
+  sendResponse(String(weight));
+}
+
+void extendActuator() {
+  digitalWrite(in1Pin, HIGH);
+  digitalWrite(in2Pin, LOW);
+  delay(10000);
+}
+
+void retractActuator() {
+  digitalWrite(in1Pin, LOW);
+  digitalWrite(in2Pin, HIGH);
+  delay(10000);
+}
+
+void spinServo() {
+  int pos = 0;
+  for (pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+}
+
+void activateBuzzer() {
+  tone(buzzer, 5000);
+  delay(3000);
+  noTone(buzzer);
+}
+
+void signalGreenLed() {
+  digitalWrite(greenLed, HIGH);
+  delay(3000);
+  digitalWrite(greenLed, LOW);
+}
+
+void signalRedLed() {
+  digitalWrite(greenLed, HIGH);
+  delay(3000);
+  digitalWrite(greenLed, LOW);
+}
+
+void turnOnFan() {
+  digitalWrite(fanRelay1, HIGH);
+  digitalWrite(fanRelay2, HIGH);
+}
+
+void turnOffFan() {
+  digitalWrite(fanRelay1, LOW);
+  digitalWrite(fanRelay2, LOW);
+}
---
+++ arduino/arduino2/arduino2.ino
@@ -0,0 +1,122 @@
+#define pulPin1 6
+#define dirPin1 7
+#define pulPin2 8
+#define dirPin2 9
+
+int stepCount = 500;
+
+const int motorRelay = 2;
+const int pulverizerRelay = 10;
+const int heatRelay = 11;
+
+String command = "";
+int current_command = -1;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pulPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(pulPin2, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
+
+  digitalWrite(dirPin1, HIGH);
+  digitalWrite(dirPin2, HIGH);
+
+  pinMode(motorRelay, OUTPUT);
+  pinMode(pulverizerRelay, OUTPUT);
+  pinMode(heatRelay, OUTPUT);
+}
+
+void loop() {
+  if(current_command == -1){
+    receiveCommand();
+  }
+
+  else if(current_command == 0){
+    activateConveyor();
+    current_command = -1;
+  }
+
+  else if(current_command == 1){
+    deactivateConveyor();
+    current_command = -1;
+  }
+
+  else if(current_command == 2){
+    activateSlicer();
+    current_command = -1;
+  }
+
+  else if(current_command == 3){
+    deactivateSlicer();
+    current_command = -1;
+  }
+
+  else if(current_command == 4){
+    activateHeat();
+    current_command = -1;
+  }
+
+  else if(current_command == 5){
+    deactivateHeat();
+    current_command = -1;
+  }
+
+  else if(current_command == 6){
+    activatePulverizer();
+    current_command = -1;
+  }
+
+  else if(current_command == 7){
+    activatePulverizer();
+    current_command = -1;
+  }
+
+}
+
+void sendResponse(String response){
+  Serial.println(response);    
+}
+ 
+void receiveCommand(){
+  if(Serial.available()){
+    int sent = Serial.readStringUntil('\n').toInt();
+    Serial.println("ok"); 
+    current_command = sent;   
+  }
+}
+
+void activateConveyor() { 
+  digitalWrite(pulPin1, HIGH);
+  digitalWrite(pulPin2, HIGH);
+}
+
+void activateSlicer(){
+  digitalWrite(motorRelay, HIGH);
+}
+
+void activateHeat(){
+  digitalWrite(heatRelay, HIGH);
+}
+
+void activatePulverizer(){
+  digitalWrite(pulverizerRelay, HIGH);
+}
+
+void deactivateConveyor() { 
+  digitalWrite(pulPin1, LOW);
+  digitalWrite(pulPin2, LOW);
+}
+
+void deactivateSlicer(){
+  digitalWrite(motorRelay, LOW);
+}
+
+void deactivateHeat(){
+  digitalWrite(heatRelay, LOW);
+}
+
+void deactivatePulverizer(){
+  digitalWrite(pulverizerRelay, LOW);
+}
