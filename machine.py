904cfb0b6ffbc09f0faa57b1bebc7ff7f15666e0
DailyLollipops <clarencemadrigal84@gmail.com>
2023-12-07 01:39:06 +0800

    Refactor machine class

--- machine.py
+++ machine.py
@@ -1,29 +1,90 @@
-from signal import signal, SIGTERM, SIGHUP
 from threading import Thread
 from serial import Serial
+from rpi_lcd import LCD
+
+import RPi.GPIO as GPIO
 import websocket
 import json
-from rpi_lcd import LCD
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-
-lcd = LCD() 
+import time
 
 class Machine:
 
     def __init__(self):
-        # Change self.ws_host to server IP address
         self.ws = None
         self.ws_host = "ws://192.168.1.25:8000/ws/socket-server/"
         self.ws_initialized = False
         
         self.arduino = Serial('/dev/ttyUSB0', 9600, timeout = 1)
         self.arduino.reset_input_buffer()
-        self.weight = '0'
-        self.temperature = '0'
-        self.start_websocket_listener()
+
         self.started = False
+        self.fan_on = False
+        self.start_websocket_listener()
+
+        self.lcd = LCD()
+
+        power_pin = 10
+        fan_pin = 12
+        self.fan_led_1 = 24
+        self.fan_led_2 = 25
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(power_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(fan_pin, GPIO.OUT)
+        GPIO.setup(self.fan_led_1, GPIO.OUT)
+        GPIO.setup(self.fan_led_2, GPIO.OUT)
+        GPIO.add_event_detect(power_pin, GPIO.RISING, callback=self._switch_state)
+        GPIO.add_event_detect(fan_pin, GPIO.RISING, callback=self._switch_fan)
       
+
+
+    #############################################
+    #                                           #
+    #               GPIO Functions              #
+    #                                           #
+    #############################################
+
+
+    def _switch_state(self, channel):
+        '''
+        Callback function for power pin
+        '''
+        self.started = not self.started
+        self.update_state(self.started)
+
+
+
+    def _switch_fan(self, channel):
+        '''
+        Callback function for fan pin
+        '''
+        self.fan_on = not self.fan_on
+        self.turn_on_fan if self.fan_on else self.turn_off_fan()
+        self._set_fan_led(self.fan_on)
+
+
+
+    def _set_fan_led(self, state: bool):
+        '''
+        Turn on/off fan led
+
+        Parameters:
+        state (bool) : Fan led state
+        '''
+        if state:
+            GPIO.output(self.fan_led_1, GPIO.HIGH)
+            GPIO.output(self.fan_led_2, GPIO.HIGH)
+        else:
+            GPIO.output(self.fan_led_1, GPIO.LOW)
+            GPIO.output(self.fan_led_2, GPIO.LOW)
+        
+
+
+    #############################################
+    #                                           #
+    #              Server Functions             #
+    #                                           #
+    #############################################
+
 
     def start_websocket_listener(self):
         '''
@@ -38,10 +99,12 @@ class Machine:
             self.ws.run_forever()
         Thread(target=_start).start()
 
+
+
     def _parse_websocket_message(self, ws, message):
         '''
-            Parse the websocket message\n
-            Just mainly for listening to state updates
+        Parse the websocket message.
+        Mainly for listening to state updates
         '''
         data = json.loads(message)
         print(data)
@@ -52,9 +115,14 @@ class Machine:
                 self.update_state(False)
                 self.ws_initialized = True
 
+
+
     def update_state(self, state: bool):
         '''
-            Update parameters and send to server
+        Update state and send to server
+
+        Parameters:
+        state (bool) : New current state
         '''
         data = {
             'type': 'update_state',
@@ -62,11 +130,16 @@ class Machine:
         }
         json_data = json.dumps(data)
         self.ws.send(json_data)
-        self.started = state
+
+
 
     def update_parameters(self, harvested: float, temperature: float):
         '''
-            Update parameters and send to server
+        Update parameters and send to server
+
+        Parameters:
+        harvested (float) : Amount currently processed
+        temperature (float) : Current temperature
         '''
         data = {
             'type': 'update_parameter',
@@ -77,9 +150,13 @@ class Machine:
         self.ws.send(json_data)
      
 
+
     def update_moisture(self, moisture: float):
         '''
-            Update moisture and send to server
+        Update moisture and send to server
+
+        Parameters:
+        moisture (float) : Current moisture level
         '''
         data = {
             'type': 'update_moisture',
@@ -88,9 +165,29 @@ class Machine:
         json_data = json.dumps(data)
         self.ws.send(json_data)
 
+
+
+    def update_fan(self, state: float):
+        '''
+        Update fan state on database
+
+        state (bool) : Fan state
+        '''
+        data = {
+            'type': 'update_fan',
+            'value': state
+        }
+        json_data = json.dumps(data)
+        self.ws.send(json_data)
+
+
+        
     def update_harvest(self, amount: float, last_temperature: float):
         '''
-            Add a new harvest record to the database
+        Add a new harvest record to the database
+
+        amount (float) : Harvest amount
+        last_temperature (float) : Last recorded temperature
         '''
         data = {
             'type': 'update_harvest',
@@ -100,80 +197,253 @@ class Machine:
         json_data = json.dumps(data)
         self.ws.send(json_data)
 
-    def safe_exit(signum, frame): 
-        exit(1)
 
-    def button_callback(self, channel):
-        self.started = not self.started
-        self.update_state(self.started)
+
+    #############################################
+    #                                           #
+    #             Arduino Functions             #
+    #                                           #
+    #############################################
+
+
+    def switch_arduino_1(self): 
+        '''
+        Switch current to Arduino 1
+        '''
+        self.arduino.close()
+        self.arduino = Serial('/dev/ttyUSB0', 9600, timeout = 1)
+        self.arduino.flush()
+
+
+
+    def switch_arduino_2(self): 
+        '''
+        Switch current to Arduino 2
+        '''
+        self.arduino.close()
+        self.arduino = Serial('/dev/ttyUSB1', 9600, timeout = 1)
+        self.arduino.flush()
+
+
 
     def send_command(self, command: int):
+        '''
+        Send command to arduino
+
+        Parameters:
+        command (int) : Command to send
+        '''
         while True:
             self.arduino.write(bytes(str(command)+'\n','utf-8'))
             response = self.get_arduino_response()
             if(response == 'ok'):
                 break
  
-    def get_arduino_response(self):
+
+
+    def get_arduino_response(self) -> str:
+        '''
+        Get response from arduino
+
+        Returns:
+        response (str) : Arduino response
+        '''
         try:
             response = self.arduino.readline().decode('utf-8').rstrip()
         except UnicodeDecodeError:
             response = self.arduino.readline().decode('utf-8').rstrip()
-        print(response)
         return response
- 
-    def lcd_show(self, message1, message2):
-        signal(SIGTERM, self.safe_exit)
-        signal(SIGHUP, self.safe_exit)
-        self.lcd.clear()
+    
 
-    def get_weight(self):
-        self.send_command(1)
+    
+    def get_temperature(self) -> float:
+        '''
+        Get current temperature from arduino
+
+        Returns:
+        temperature (float) : Temperature
+        '''
+        self.send_command(11)
+        response = self.get_arduino_response()
+        while not response:
+            response = self.get_arduino_response()
+        temperature = float(response)
+        return temperature
+
+
+
+    def get_moisture(self) -> float:
+        '''
+        Get current moisture from arduino
+
+        Returns:
+        moisture (float) : Moisture
+        '''
+        self.send_command(11)
+        response = self.get_arduino_response()
+        while not response:
+            response = self.get_arduino_response()
+        moisture = float(response)
+        return moisture
+    
+
+
+    def get_weight(self) -> float:
+        '''
+        Get current weight from arduino
+
+        Returns:
+        weight (float) : Weight
+        '''
+        self.send_command(13)
         response = self.get_arduino_response()
         while not response:
             response = self.get_arduino_response()
         weight = float(response)
         return weight
     
-    def get_temperature(self):
-        self.send_command(3)
-        temperature = float(self.get_arduino_response())
-        return temperature
+
+    def get_harvest_weight(self) -> float:
+        '''
+        Get harvest weight from arduino
+
+        Returns:
+        weight (float) : Harvest weight
+        '''
+        self.send_command(14)
+        response = self.get_arduino_response()
+        while not response:
+            response = self.get_arduino_response()
+        weight = float(response)
+        return weight
     
-    def activate_actuator(self):
-        self.send_command(2)
 
-    def activate_slicer(self):
-        self.send_command(4)
 
-    def activate_conveyor(self):
-        self.send_command(5)
+    def extend_actuator(self):
+        '''
+        Explicit function for extendActuator on Arduino
+        '''
+        self.send_command(15)
 
-    def activate_pulvurizer(self):
-        self.send_command(6)
 
-    def get_moisture(self):
-        self.send_command(7)
-
-    def activate_extraheat(self):
-        self.send_command(8)
-
-    def off_pulvurizer(self):
-        self.send_command(9)
 
     def retract_actuator(self):
-        self.send_command(10)
+        '''
+        Explicit function for retractActuator on Arduino
+        '''
+        self.send_command(16)
 
-    def get_wfinish(self):
-        self.send_command(12)
-        #finish
 
-    def switch_arduino_1(self): 
-        self.arduino.close()
-        self.arduino = Serial('/dev/ttyUSB1', 9600, timeout = 1)
-        self.arduino.flush()
 
-    def switch_arduino_0(self): 
-        self.arduino.close()
-        self.arduino = Serial('/dev/ttyUSB0', 9600, timeout = 1)
-        self.arduino.flush()
+    def turn_on_fan(self):
+        '''
+        Explicit function for turnOnFan on Arduino
+        '''
+        self.send_command(17)
+
+
+
+    def turn_off_fan(self):
+        '''
+        Explicit function for turnOffFan on Arduino
+        '''
+        self.send_command(18)
+
+
+
+    def activate_buzzer(self):
+        '''
+        Explicit function for activateBuzzer on Arduino
+        '''
+        self.send_command(19)
+        time.sleep(3.5)
+
+
+
+    def signal_green_led(self):
+        '''
+        Explicit function for signalGreenLed on Arduino
+        '''
+        self.send_command(20)
+        time.sleep(3.5)
+
+
+
+    def signal_red_led(self):
+        '''
+        Explicit function for signalRedLed on Arduino
+        '''
+        self.send_command(21)
+        time.sleep(3.5)
+
+
+
+    def spin_servo(self):
+        '''
+        Explicit function for spinServo on Arduino
+        '''
+        self.send_command(22)
+        time.sleep(6)
+
+
+
+    def activate_conveyor(self):
+        '''
+        Explicit function for activateConveyor on Arduino
+        '''
+        self.send_command(0)
+
+
+
+    def deactivate_conveyor(self):
+        '''
+        Explicit function for deactivateConveyor on Arduino
+        '''
+        self.send_command(1)
+
+
+    def activate_slicer(self):
+        '''
+        Explicit function for activateSlicer on Arduino
+        '''
+        self.send_command(2)
+
+
+
+    def deactivate_slicer(self):
+        '''
+        Explicit function for deactivateSlicer on Arduino
+        '''
+        self.send_command(3)
+
+
+
+    def activate_heat(self):
+        '''
+        Explicit function for activateHeat on Arduino
+        '''
+        self.send_command(4)
+
+
+
+    def deactivate_heat(self):
+        '''
+        Explicit function for deactivateHeat on Arduino
+        '''
+        self.send_command(5)
+
+
+
+    def activate_pulverizer(self):
+        '''
+        Explicit function for activatePulverizer on Arduino
+        '''
+        self.send_command(6)
+
+
+
+    def deactivate_pulverizer(self):
+        '''
+        Explicit function for deactivatePulverizer on Arduino
+        '''
+        self.send_command(7)
