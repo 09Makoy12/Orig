cd2982f8e72b805fe86793c4a287279a656bd953
johnromie <johnromiematre@gmail.com>
2023-12-09 03:49:38 +0800

    heater

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from machine import Machine
 import time
 
-if __name__ == '__main__': 
-
+if __name__ == '__main__':
     machine = Machine()
 
-    machine.get_temperature()
+  
+    machine.get_weight()
